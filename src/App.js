caa7115d46789d147bcef3f7bc32a58fd6f8ba57
Moganesan <developer.moganesan@gmail.com>
2021-11-13 17:50:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+	<h2>Welcome Moganesan</h2>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
